ab3e02fcf784d5bd373961e05ad6d357ef946e04
Aotokitsuruya <elct9620@aotoki.me>
2023-04-22 19:04:40 +0800

    Add full go related tools support

--- overlays/20_go.nix
+++ overlays/20_go.nix
@@ -1,11 +1,32 @@
 self: super:
 with super;
 {
-  auraGo = buildEnv {
+  # Reference: https://github.com/svend/user-env-nix/blob/main/overlays/gotools.nix
+  gotools = super.gotools.overrideAttrs (old: rec {
+    # Do not install things like doc, link, and fix
+    postConfigure = ''
+      # # Make the builtin tools available here
+      # mkdir -p $out/bin
+      # eval $(go env | grep GOTOOLDIR)
+      # find $GOTOOLDIR -type f | while read x; do
+      #   ln -sv "$x" "$out/bin"
+      # done
+      # export GOTOOLDIR=$out/bin
+    '';
+
+    # Do not install bundle (conflicts with ruby bundler)
+    excludedPackages = with super; "\\("
+    + pkgs.lib.concatStringsSep "\\|" ([ "bundle" "testdata" "vet" "cover" ])
+    + "\\)";
+  });
+
+  auraGo = with self; buildEnv {
     name = "aura-go";
     paths = [
       go
       gopls
+      gotools
+      gotags
     ];
   };
 }
