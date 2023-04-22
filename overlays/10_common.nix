c55d30aecd39d3b97bf1030000a6ae12b520519a
Aotokitsuruya <elct9620@aotoki.me>
2023-04-22 13:24:57 +0800

    Add powerline-go

--- overlays/10_common.nix
+++ overlays/10_common.nix
@@ -4,14 +4,19 @@ with super;
  auraCommon = buildEnv {
    name = "aura-common";
    paths = [
+     # Text
      bat
+     # Editor
+     vimWithConfig
+     # Shell
      coreutils
+     tmuxWithConfig
+     powerline-go
+     # Development
      git
+     # System
      htop
      reattach-to-user-namespace
-     tmuxWithConfig
-     vimWithConfig
-     zsh
    ];
  };
 }
