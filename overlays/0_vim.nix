aecb5f43837332b82f71cbb31191fac0c06d3b52
Aotokitsuruya <elct9620@aotoki.me>
2023-04-26 10:04:50 +0800

    Keep vimrc in file instead of overlays

--- overlays/0_vim.nix
+++ overlays/0_vim.nix
@@ -10,22 +10,7 @@ with super;
     name = "vimWithConfig";
     paths = [
       (self.vim-full.customize {
-        vimrcConfig.customRC = ''
-        " Install vim-plug if we don't already have it
-        if empty(glob("$HOME/.vim/autoload/plug.vim"))
-            " Ensure all needed directories exist  (Thanks @kapadiamush)
-            execute '!mkdir -p ~/.vim/plugged'
-            execute '!mkdir -p ~/.vim/autoload'
-            " Download the actual plugin manager
-            execute '!curl -fLo ~/.vim/autoload/plug.vim https://raw.github.com/junegunn/vim-plug/master/plug.vim'
-        endif
-
-        " Load Plug
-        call plug#begin('~/.vim/plugged')
-        source $HOME/.vim/Plug
-        filetype plugin indent on
-        call plug#end()
-        '';
+        vimrcConfig.customRC = builtins.readFile ../config/vimrc;
       })
     ];
   };
