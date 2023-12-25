5939351c9900e56b3ce3b8193411688199bfc315
Aotokitsuruya <contact@aotoki.me>
2023-12-25 16:48:44 +0800

    Update rbenv to v20231225 to support Ruby 3.3

--- packages/ruby-build.nix
+++ packages/ruby-build.nix
@@ -4,7 +4,7 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "ruby-build";
-  version = "20231114";
+  version = "20231225";
 
   src = fetchFromGitHub {
     owner = "rbenv";
@@ -12,7 +12,7 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     # NOTE:  Calculate sha256
     # nix-shell -p nix-prefetch-git jq --run "nix hash to-sri sha256:\$(nix-prefetch-git --url https://github.com/rbenv/ruby-build --quiet --rev v20230424 | jq -r '.sha256')"
-    sha256 = "sha256-dsmBhevcYWTajnt3aQO6qloQhuMt22mnn4mFW2gQBIQ=";
+    sha256 = "sha256-kqSklA0bgorK62GdV2xXIBBOfVRIoRia7xgsYH6ZYIw=";
   };
 
   nativeBuildInputs = [
