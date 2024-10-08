b74f18eeee4f84ea59d785331575a10488d611aa
Aotokitsuruya <contact@aotoki.me>
2024-10-08 09:13:45 +0800

    feat(deps): update ruby-build to v20241007

--- packages/ruby-build.nix
+++ packages/ruby-build.nix
@@ -4,15 +4,15 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "ruby-build";
-  version = "20240917";
+  version = "v20241007";
 
   src = fetchFromGitHub {
     owner = "rbenv";
     repo = "ruby-build";
-    rev = "v${version}";
+    rev = "${version}";
     # NOTE:  Calculate sha256
     # nix-shell -p nix-prefetch-git jq --run "nix hash to-sri sha256:\$(nix-prefetch-git --url https://github.com/rbenv/ruby-build --quiet --rev v20230424 | jq -r '.sha256')"
-    sha256 = "sha256-qeSBulxb9JcJuC1fnw5haf7b2QSDm1qnHC3ZHdxwsDI=";
+    sha256 = "sha256-R66O4EeVZqznKA9p0uFCfVpeUUf4tKjjncGnIKJwoBs=";
   };
 
   nativeBuildInputs = [
