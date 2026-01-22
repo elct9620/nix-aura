b9f4471b3dac6c6f29dc5d29e3feeb306642a78f
Aotokitsuruya <contact@aotoki.me>
2026-01-22 19:28:34 +0800

    chore: bump ruby-build to v20261221

--- packages/ruby-build.nix
+++ packages/ruby-build.nix
@@ -4,7 +4,7 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "ruby-build";
-  version = "v20260114";
+  version = "v20260121";
 
   src = fetchFromGitHub {
     owner = "rbenv";
@@ -12,7 +12,7 @@ stdenv.mkDerivation rec {
     rev = "${version}";
     # NOTE:  Calculate sha256
     # make ruby-build
-    sha256 = "sha256-kCKCLicAHhtkIgc5RoxGQh8SGAlABSJI74EnT8pdmDs=";
+    sha256 = "sha256-ThVmAPf+IRYw765crGXfcPekfEYdoSm6Ubo6J2dgYcg=";
   };
 
   nativeBuildInputs = [
