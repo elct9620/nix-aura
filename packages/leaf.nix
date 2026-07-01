96f75101136f86f8d98722c8b7999040dd33274c
Aotokitsuruya <contact@aotoki.me>
2026-07-01 20:30:51 +0800

    chore(leaf): bump leaf to 1.25.0

--- packages/leaf.nix
+++ packages/leaf.nix
@@ -2,16 +2,16 @@
 with pkgs;
 rustPlatform.buildRustPackage rec {
   pname = "leaf";
-  version = "1.24.2";
+  version = "1.25.0";
 
   src = fetchFromGitHub {
     owner = "RivoLink";
     repo = "leaf";
     rev = "${version}";
-    sha256 = "sha256-mKB3x7HaO48uMzxaKpep+69D52RgIKTtvVNdm/EOJaU=";
+    sha256 = "sha256-OSx797tkwjKU9j+0AhQIT7uLM75PzHVw12d5LG6vT3Q=";
   };
 
-  cargoHash = "sha256-GPCU2L3gDj4QOlg3MJ+OndnM2P0jUA+cIuSOhEEZHrU=";
+  cargoHash = "sha256-rEISBL5vWYP5UKFKWLA2RxlqDBFTz8qPpiPOfxeNUFQ=";
 
   nativeBuildInputs = [ pkg-config ];
 
